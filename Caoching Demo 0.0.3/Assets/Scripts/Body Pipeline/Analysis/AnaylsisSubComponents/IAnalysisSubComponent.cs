2c0bc81f65a6277d97fc49fb47f4de9b813a02da
Mohammed Haider <mrhaider@gmail.com>
2016-04-22 18:20:46 -0400

    renamed overly complicated named.

---
+++ Caoching Demo 0.0.3/Assets/Scripts/Body Pipeline/Analysis/AnaylsisSubComponents/IAnalysisSubComponent.cs
@@ -0,0 +1,17 @@
+﻿/** 
+* @file IAnalysisSubComponent.cs
+* @brief Contains the IAnalysisSubComponent  interface
+* @author Mohammed Haider (mohammed@heddoko.com)
+* @date April 2016
+* Copyright Heddoko(TM) 2016, all rights reserved
+*/
+namespace Assets.Scripts.Body_Pipeline.Analysis.AnaylsisSubComponents
+{
+    /// <summary>
+    /// Provides an interface for Analysis Sub components(REBA/RULA SCORE,NIOSH, SNOOKTABLE, ETC)
+    /// </summary>
+    public interface IAnalysisSubComponent
+    {
+         
+    }
+}
