cc7c74fcec50dcbec61ac1c6cc696fd054c199cf
Simon Corbeil <simon@heddoko.com>
2016-04-26 17:31:22 -0400

    First implemetation of calibration algorithm- shiu (code need to be corrected to follow the naming rules)

--- CalibrationLib/Calibration1/CalibrationTest/Program.cs
+++ CalibrationLib/Calibration1/CalibrationTest/Program.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using Calibration1.CalibrationTransformation;
+using UnityEngine;
 
 namespace CalibrationTest
 {
     class Program
     {
         static void Main(string[] args)
-        {
-
+        {          
+          float phi1 = 0.0F;
+          float psy1 = 0.0F;
+          float theta1 = 0.0F;
+          float phi2 = 0.0F;
+          float psy2 = 0.0F;
+          float theta2 = 0.0F;
+          Matrix4x4 X  = Matrix4x4.zero;
+          ShiuTransform Cal = new ShiuTransform() ;
+          X = Cal.Shiufunc(phi1, psy1, theta1, phi2, psy2, theta2);
+          Console.ReadLine();
         }
     }
 }
