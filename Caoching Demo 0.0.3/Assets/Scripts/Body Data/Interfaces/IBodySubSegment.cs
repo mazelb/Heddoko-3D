a3d7ecf9d842d946187e81d82e5c8461c25db52d
Mohammed Haider <mrhaider@gmail.c>
2016-05-04 10:53:22 -0400

    Added function signature in IBodySubSegments to accept quaternions of the form x,y,z,w

--- Caoching Demo 0.0.3/Assets/Scripts/Body Data/Interfaces/IBodySubSegment.cs
+++ Caoching Demo 0.0.3/Assets/Scripts/Body Data/Interfaces/IBodySubSegment.cs
@@ -8,12 +8,36 @@
 namespace Assets.Scripts.Body_Data.Interfaces
 {
     /// <summary>
-    /// A component for Body Sub Segments
+    /// A component for Body SubSegments
     /// </summary>
     public interface IBodySubsegment
     {
         BodyStructureMap.SubSegmentTypes SubSegmentType { get; }
         BodyStructureMap.SubSegmentOrientationType SubsegmentOrientationType { get; }
 
+        /// <summary>
+        /// The current subsegment transforms values
+        /// </summary> 
+        IBodySubsegmentTransformValues SubsegmentTransformValues { get; }
+
+        /// <summary>
+        /// Reset's the Subsegment's transform values 
+        /// </summary>
+        void Reset();
+
+        /// <summary>
+        ///  Updates the Subsegment's orientation with respect to its quaternion values
+        /// </summary>
+        /// <param name="vX">X component of the quaternion</param>
+        /// <param name="vY">Y component of the quaternion</param>
+        /// <param name="vZ">Z component of the quaternion</param>
+        /// <param name="vW">W component of the quaternion</param>
+        /// <param name="vApplyLocal">apply the rotation on a local level or global level?</param>
+        /// <param name="vResetRotation">reset the rotation?</param>
+        void UpdateSubsegmentOrientation(float vX, float vY, float vZ, float vW, int vApplyLocal = 0,
+            bool vResetRotation = false);
+
+        
+ 
     }
 }
