d543403937cb60c6c5b51964871bf11891a5cd6f
Simon Corbeil <simon@heddoko.com>
2016-05-24 08:28:54 -0400

    Simple modification

--- CalibrationLib/Calibration1/CalibrationTest/Program.cs
+++ CalibrationLib/Calibration1/CalibrationTest/Program.cs
@@ -5,15 +5,17 @@ using Calibration1.CalibrationTransformation;
 using MathNet.Numerics.LinearAlgebra;
 using MathNet.Numerics.Distributions;
 using UnityEngine;
+using System.IO;
 namespace CalibrationTest
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Test_ShiuTransform();
+            Test1_ShiuTransform(); //from raw data;
+            //Test_ShiuTransform_From_RawData();
         }
-        static public void Test_ShiuTransform()
+        static public void Test1_ShiuTransform()
         {
             bool Test = false;
             Vector<float> RawSensDataAngles1 = Vector<float>.Build.Dense(3, 0);
@@ -24,46 +26,78 @@ namespace CalibrationTest
             Vector<float> rand2 = Vector<float>.Build.Dense(3, 0);
             ShiuTransform AvatarToDataSensorsTransform = new ShiuTransform();
             Matrix<float> M = Matrix<float>.Build.Dense(3, 2, 0);
-            M = Case("Case1upperarm");
-            RawSensDataAngles1[0] = M[0, 0];
-            RawSensDataAngles1[1] = M[1, 0];
-            RawSensDataAngles1[2] = M[2, 0];
-            RawSensDataAngles2[0] = M[0, 1];
-            RawSensDataAngles2[1] = M[1, 1];
-            RawSensDataAngles2[2] = M[2, 1];
-            Xtmp = AvatarToDataSensorsTransform.Shiufunc(RawSensDataAngles1, RawSensDataAngles2);
-            Console.WriteLine("----------------------------------------------------------------");
+            /* M = Case("Case1upperarm");
+             RawSensDataAngles1 = M.Column(0);
+             RawSensDataAngles2 = M.Column(1);
+             Xtmp = AvatarToDataSensorsTransform.Shiufunc(RawSensDataAngles1, RawSensDataAngles2);
+             Console.WriteLine("----------------------------------------------------------------");
+             Console.ReadLine();
+             M = Case("Case2upperarm");
+             RawSensDataAngles1 = M.Column(0);
+             RawSensDataAngles2 = M.Column(1);
+             Xtmp = AvatarToDataSensorsTransform.Shiufunc(RawSensDataAngles1, RawSensDataAngles2);
+             Console.WriteLine("----------------------------------------------------------------");
+             Console.ReadLine();*/
+             M = Case("Case1forearm");
+             RawSensDataAngles1 = M.Column(0);
+             RawSensDataAngles2 = M.Column(1);
+             Xtmp = AvatarToDataSensorsTransform.Shiufunc(RawSensDataAngles1, RawSensDataAngles2);
+             Console.WriteLine("----------------------------------------------------------------");
+             Console.ReadLine();
+            /* M = Case("Case2forearm");
+             RawSensDataAngles1 = M.Column(0);
+             RawSensDataAngles2 = M.Column(1);
+             Xtmp = AvatarToDataSensorsTransform.Shiufunc(RawSensDataAngles1, RawSensDataAngles2);
+             Console.WriteLine("----------------------------------------------------------------");
+             Console.ReadLine();*/
+        }
+        static public void Test_ShiuTransform_From_RawData()
+        {
+            ShiuTransform AvatarToDataSensorsTransform = new ShiuTransform();
+            string namefileZ = "C:\\Users\\Simon\\Documents\\Sython\\RawDA\\Zpose.txt";
+            string namefileS = "C:\\Users\\Simon\\Documents\\Sython\\RawDA\\Spose.txt";
+            Vector<float> RawSensDataAngles1 = Vector<float>.Build.Dense(3, 0);
+            Vector<float> RawSensDataAngles2 = Vector<float>.Build.Dense(3, 0);
+            Matrix<float> Angles = Program.Read_RawData(namefileZ, namefileS);
+            Console.WriteLine(Angles);
+            //Matrix<float> X;
+            Matrix<float> R = Matrix<float>.Build.Dense(3,3,0);
+            for (int i = 0; i < 3; i++)
+            {
+                RawSensDataAngles1 = Angles.Column(i, 0, 3);
+                RawSensDataAngles2 = Angles.Column(i, 3, 3);
+                R += AvatarToDataSensorsTransform.Shiufunc(RawSensDataAngles1, RawSensDataAngles2);
+                Console.WriteLine("------Rmoy-------");
+                ShiuTransform.Print(R/(i+1.0F));
+            }            
             Console.ReadLine();
-            M = Case("Case2upperarm");
-            RawSensDataAngles1[0] = M[0, 0];
-            RawSensDataAngles1[1] = M[1, 0];
-            RawSensDataAngles1[2] = M[2, 0];
-            RawSensDataAngles2[0] = M[0, 1];
-            RawSensDataAngles2[1] = M[1, 1];
-            RawSensDataAngles2[2] = M[2, 1];
-            Xtmp = AvatarToDataSensorsTransform.Shiufunc(RawSensDataAngles1, RawSensDataAngles2);
-            Console.WriteLine("----------------------------------------------------------------");
-            Console.ReadLine();
-            M = Case("Case1upperarm");
-            RawSensDataAngles1[0] = M[0, 0];
-            RawSensDataAngles1[1] = M[1, 0];
-            RawSensDataAngles1[2] = M[2, 0];
-            RawSensDataAngles2[0] = M[0, 1];
-            RawSensDataAngles2[1] = M[1, 1];
-            RawSensDataAngles2[2] = M[2, 1];
-            Xtmp = AvatarToDataSensorsTransform.Shiufunc(RawSensDataAngles1, RawSensDataAngles2);
-            Console.WriteLine("----------------------------------------------------------------");
-            Console.ReadLine();
-            M = Case("Case2upperarm");
-            RawSensDataAngles1[0] = M[0, 0];
-            RawSensDataAngles1[1] = M[1, 0];
-            RawSensDataAngles1[2] = M[2, 0];
-            RawSensDataAngles2[0] = M[0, 1];
-            RawSensDataAngles2[1] = M[1, 1];
-            RawSensDataAngles2[2] = M[2, 1];
-            Xtmp = AvatarToDataSensorsTransform.Shiufunc(RawSensDataAngles1, RawSensDataAngles2);
-            Console.WriteLine("----------------------------------------------------------------");
-            Console.ReadLine();
+        }
+        static public Matrix<float> Read_RawData(string NameFilePoseZ, string NameFilePoseS)
+        {
+            StreamReader reader = File.OpenText(NameFilePoseZ);
+            string line = reader.ReadLine();
+            string[] numbers = line.Split(',');
+            Matrix<float> Angles = Matrix<float>.Build.Dense(6, 3, 0);
+            //Zpose       
+            for (int i = 0; i < 3; i++)//i:angles (X,Y,Z)
+            {
+                for (int j = 0; j < 3; j++)//j:angles values (start,mean,end) 
+                {
+                    Angles[i, j] = float.Parse(numbers[j + 3 * i]);
+                }
+            }
+            reader = File.OpenText(NameFilePoseS);
+            line = reader.ReadLine();
+            numbers = line.Split(',');
+            //Spose       
+            for (int i = 0; i < 3; i++)//i:angles (X,Y,Z)
+            {
+                for (int j = 0; j < 3; j++)//j:angles values (start,mean,end) 
+                {
+                    Angles[3 + i, j] = float.Parse(numbers[j + 3 * i]);
+                }
+            }
+            return Angles;
         }
         static public Matrix<float> Case(string s)
         {
@@ -86,7 +120,7 @@ namespace CalibrationTest
                 V[1, 1] = 1.247436F;
                 V[2, 1] = 0.484131F;
             }
-            else if (s == "Case1forerarm")
+            else if (s == "Case1forearm")
             {
                 V[0, 0] = -0.062500F;
                 V[1, 0] = -0.058350F;
@@ -95,7 +129,7 @@ namespace CalibrationTest
                 V[1, 1] = 1.305542F;
                 V[2, 1] = 0.889282F;
             }
-            else if (s == "Case2forerarm")
+            else if (s == "Case2forearm")
             {
                 V[0, 0] = -0.077510F;
                 V[1, 0] = -0.101810F;
@@ -108,9 +142,6 @@ namespace CalibrationTest
         }
     }
 }
-
-
-
 
 /*
 RawSensDataAngles1 = AvatarToDataSensorsTransform.PoseToEulerAngles(ShiuTransform.pose1);
