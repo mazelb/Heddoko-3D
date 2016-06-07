a04db156f23ecb0eb98e3d9af2486f37ddefb060
Mohammed Haider <mrhaider@gmail.com>
2016-06-07 11:00:10 -0400

    Added protobuf decoding mechanism in the library

---
+++ HeddokoLib/HeddokoLib/adt/EventSet.cs
@@ -0,0 +1,86 @@
+﻿// /**
+// * @file EventSet.cs
+// * @brief Contains the 
+// * @author Mohammed Haider( mohammed@heddoko.com)
+// * @date 06 2016
+// * Copyright Heddoko(TM) 2016,  all rights reserved
+// */
+
+using System;
+using System.Collections.Generic;
+
+namespace HeddokoLib.adt
+{
+    /// <summary>
+    /// this class is to provide type safety and code maintainability when using EventSet
+    /// </summary>
+    public sealed class EventKey : Object
+    {
+        
+    }
+
+    /// <summary>
+    /// A Data structure that holds a set of event handlers
+    /// </summary>
+    public class EventSet
+    {
+        /// <summary>
+        /// The set of events, mapped by their specifed event keys
+        /// </summary>
+        private readonly Dictionary<EventKey, Delegate> mEventSet = new Dictionary<EventKey, Delegate>();
+
+        /// <summary>
+        /// Adds an event handler by the specified key
+        /// </summary>
+        /// <param name="vKey"></param>
+        /// <param name="vHandler"></param>
+        public void Add(EventKey vKey, Delegate vHandler)
+        {
+            Delegate vDel;
+            mEventSet.TryGetValue(vKey, out vDel);
+            mEventSet[vKey] =Delegate.Combine(vDel,vHandler);
+        }
+
+        /// <summary>
+        /// Removes an event handler by the specified key
+        /// </summary> 
+        /// <param name="vKey">The key</param>
+        /// <param name="vHandler">The event handler</param>
+        public void Remove(EventKey vKey, Delegate vHandler)
+        {
+            Delegate vDel;
+            if (mEventSet.TryGetValue(vKey, out vDel))
+            {
+                vDel = Delegate.Remove(vDel,vHandler);
+            }
+
+            //after remove, verify if there are no more elements to be found at the specified key. 
+            if (vDel != null)
+            {
+                mEventSet[vKey] = vDel;
+            }
+            else
+            {
+                mEventSet.Remove(vKey);
+            }
+        }
+
+        /// <summary>
+        /// Raise the event for the specified key
+        /// </summary>
+        /// <param name="vKey"></param>
+        /// <param name="vSender"></param>
+        /// <param name="vEventArgs"></param>
+        public void Raise(EventKey vKey, Object vSender, EventArgs vEventArgs)
+        {
+            Delegate vDel;
+            mEventSet.TryGetValue(vKey, out vDel);
+            if (vDel != null)
+            {
+                vDel.DynamicInvoke(vSender, vEventArgs);
+            }
+        }
+
+
+    }
+}
---
+++ HeddokoLib/HeddokoLib/heddokoProtobuff/Decoder/ProtofileDecoder.cs
@@ -0,0 +1,60 @@
+﻿// /**
+// * @file ProtofileDecoder.cs
+// * @brief Contains the 
+// * @author Mohammed Haider(mohammed@heddoko.com)
+// * @date June 2016
+// * Copyright Heddoko(TM) 2016,  all rights reserved
+// */
+
+using System.Net;
+
+namespace HeddokoLib.heddokoProtobuff.Decoder
+{
+    public delegate void FileCompletedEvent(object vSender, object vArgs);
+
+    /// <summary>
+    /// A protofile decoder. 
+    /// </summary>
+    public class ProtofileDecoder
+    {
+        /// <summary>
+        /// File read completion event
+        /// </summary>
+        private event FileCompletedEvent mFileCompletedEvent;
+        /// <summary>
+        /// A Protoframe decoder
+        /// </summary>
+        private ProtoFrameDecoder mFrameDecoder =new ProtoFrameDecoder();
+ 
+        /// <summary>
+        /// Registers a handler
+        /// </summary>
+        /// <param name="vHandler"></param>
+        public void RegisterFileCompletedEventHandler(FileCompletedEvent vHandler)
+        {
+            mFileCompletedEvent += vHandler;
+        }
+
+        /// <summary>
+        /// Removes a file completed event handler
+        /// </summary>
+        /// <param name="vHandler"></param>
+        public void DeRegisterFileCompletedEventHandler(FileCompletedEvent vHandler)
+        {
+            mFileCompletedEvent -= vHandler;
+        }
+
+        /// <summary>
+        /// Reads a Heddoko protobuf file
+        /// </summary>
+        /// <param name="vPath"></param>
+        public void ReadFile(string vPath)
+        {
+            
+        }
+
+
+    }
+
+   
+}
