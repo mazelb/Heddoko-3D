bf3bb992f69ff2a4cfb8594f1eb9f9a53fd59cdf
Mohammed Haider <mrhaider@gmail.com>
2016-06-15 15:50:58 -0400

    added a AppMain_quatDebug scene to the project. Added a quaternion body segment flag setter. Set's the bodysegment flag according to a Toggle.

--- Caoching Demo 0.0.3/Assets/Scripts/Tests/BodySegmentQuaternionFlagSetter.cs
+++ Caoching Demo 0.0.3/Assets/Scripts/Tests/BodySegmentQuaternionFlagSetter.cs
@@ -1,14 +1,32 @@
-﻿// /**
-// * @file BodySegmentQuaternionFlagSetter.cs
-// * @brief Contains the 
-// * @author Mohammed Haider( 
-// * @date 06 2016
-// * Copyright Heddoko(TM) 2016,  all rights reserved
-// */
+﻿/**
+* @file BodySegmentQuaternionFlagSetter.cs
+* @brief Contains the BodySegmentQuaternionFlagSetter
+* @author Mohammed Haider( mohammed@heddoko.com)
+* @date June 2016
+* Copyright Heddoko(TM) 2016,  all rights reserved
+*/
+
+using UnityEngine;
+using UnityEngine.UI;
+
 namespace Assets.Scripts.Tests
 {
-    public class BodySegmentQuaternionFlagSetter
+    public class BodySegmentQuaternionFlagSetter:MonoBehaviour
     {
-         
+        public Toggle Toggle;
+
+        void Awake()
+        {
+            Toggle.onValueChanged.AddListener(  SetBodySegmentFlag);
+        }
+
+        /// <summary>
+        /// Sets the BodySegment's using Quaternion flag to vX
+        /// </summary>
+        /// <param name="vX"></param>
+        void SetBodySegmentFlag(bool vX)
+        {
+            BodySegment.GBodyFrameUsingQuaternion = vX;
+        }
     }
 }
