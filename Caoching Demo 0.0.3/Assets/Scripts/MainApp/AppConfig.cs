1acf39b4c70107aa4a0b4fe33360ae77c6683c6d
HeddokoBuilderBot <buildermachine@heddoko.com>
2016-09-19 16:23:22 -0400

    Automated commit+ message: updated to version:...

--- Caoching Demo 0.0.3/Assets/Scripts/MainApp/AppConfig.cs
+++ Caoching Demo 0.0.3/Assets/Scripts/MainApp/AppConfig.cs
@@ -3,7 +3,7 @@ public static class AppConfig
     ///<summary>
     ///The application version
     ///</summary>
-    private static string sVersion = 1.0.9.0;
+    private static string sVersion = 1.0.10.0;
     public static string Version
     {
         get
