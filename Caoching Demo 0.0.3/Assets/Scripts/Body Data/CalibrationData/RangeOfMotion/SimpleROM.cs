55b88628e4d53ac36c00e00419e7aff3a6d78cc3
Jeremie Kaltenmark <kaltenmark.jeremie@cdrin.com>
2016-10-20 11:51:33 -0400

    [TASK] base for ROM

---
+++ Caoching Demo 0.0.3/Assets/Scripts/Body Data/CalibrationData/RangeOfMotion/SimpleROM.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+using MathNet.Numerics;
+
+namespace Assets.Scripts.Body_Data.CalibrationData.RangeOfMotion
+{
+    //enum ROM_LOCAL_AXE { X = 1, Y = 2, Z = 4, LOCK = 0 };
+    enum ROM_LOCAL_AXE { PITCH = 1, YAW = 2, ROLL = 4, LOCK = 0 };
+
+    public class AngleConstraint
+    {
+        public AngleConstraint() { }
+        public AngleConstraint(Vector3 aAxe, float aMin, float aMax) { axe = aAxe; minAngle = aMin; maxAngle = aMax; }
+
+        public Vector3 axe;
+        public float minAngle;
+        public float maxAngle;
+    }
+
+    public class SimpleROM
+    {
+        //public enum ROM_LOCAL_AXE { X = 1, Y = 2, Z = 4, LOCK = 0 };
+
+        //private ROM_LOCAL_AXE m_axe;
+        //public ROM_LOCAL_AXE Axe
+        //{
+        //    get
+        //    { return m_axe; }
+        //    set
+        //    {
+        //        //enforce value to be a bit mask
+        //        m_axe = ROM_LOCAL_AXE.LOCK;
+
+        //        if (((value & ROM_LOCAL_AXE.X) == ROM_LOCAL_AXE.X))
+        //        {
+        //            m_axe = m_axe | ROM_LOCAL_AXE.X;
+        //        }
+        //        if (((value & ROM_LOCAL_AXE.Y) == ROM_LOCAL_AXE.Y))
+        //        {
+        //            m_axe = m_axe | ROM_LOCAL_AXE.Y;
+        //        }
+        //        if (((value & ROM_LOCAL_AXE.Z) == ROM_LOCAL_AXE.Z))
+        //        {
+        //            m_axe = m_axe | ROM_LOCAL_AXE.Z;
+        //        }
+        //    }
+        //}
+
+        public void SetPitchMinMax(float min, float max)
+        {
+            if (PitchMinMax == null)
+            {
+                PitchMinMax = new AngleConstraint(Vector3.right, min, max);
+            }
+            else
+            {
+                PitchMinMax.minAngle = min;
+                PitchMinMax.maxAngle = max;
+
+            }
+        }
+        public void SetYawMinMax(float min, float max)
+        {
+            if (PitchMinMax == null)
+            {
+                PitchMinMax = new AngleConstraint(Vector3.up, min, max);
+            }
+            else
+            {
+                PitchMinMax.minAngle = min;
+                PitchMinMax.maxAngle = max;
+            }
+        }
+        public void SetRollMinMax(float min, float max)
+        {
+            if (PitchMinMax == null)
+            {
+                PitchMinMax = new AngleConstraint(Vector3.forward, min, max);
+            }
+            else
+            {
+                PitchMinMax.minAngle = min;
+                PitchMinMax.maxAngle = max;
+
+            }
+        }
+
+        public AngleConstraint PitchMinMax = null;
+        public AngleConstraint YawMinMax   = null;
+        public AngleConstraint RollMinMax  = null;
+    }
+}
---
+++ Caoching Demo 0.0.3/Assets/Scripts/Body Data/CalibrationData/RangeOfMotion/StaticROM.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Assets.Scripts.Body_Data.CalibrationData.RangeOfMotion
+{
+    public class StaticROM
+    {
+        public Dictionary<BodyStructureMap.SubSegmentTypes, SimpleROM> squeletteRom = new Dictionary<BodyStructureMap.SubSegmentTypes, SimpleROM>();
+
+        public StaticROM()
+        {
+            SimpleROM t_UpperSpine = squeletteRom[BodyStructureMap.SubSegmentTypes.SubsegmentType_UpperSpine] ;
+            t_UpperSpine.SetPitchMinMax(-40,40);
+            t_UpperSpine.SetYawMinMax(-40,40);
+            t_UpperSpine.SetRollMinMax(-40, 40);
+            SimpleROM t_LowerSpine = squeletteRom[BodyStructureMap.SubSegmentTypes.SubsegmentType_LowerSpine];
+            t_LowerSpine.SetPitchMinMax(-40, 40);
+            t_LowerSpine.SetYawMinMax(-40, 40);
+            t_LowerSpine.SetRollMinMax(-40, 40);
+
+            SimpleROM t_RightUpperArm = squeletteRom[BodyStructureMap.SubSegmentTypes.SubsegmentType_RightUpperArm];
+            t_RightUpperArm.SetPitchMinMax(-60, 100); // up/down
+            t_RightUpperArm.SetYawMinMax(-100, 100);  // front/back  
+            t_RightUpperArm.SetRollMinMax(-90, 90);   // twist
+            SimpleROM t_RightForeArm = squeletteRom[BodyStructureMap.SubSegmentTypes.SubsegmentType_RightForeArm];
+            t_RightForeArm.SetYawMinMax(-80, 80);  // flex
+            t_RightForeArm.SetRollMinMax(-20, 150);   // twist
+
+            SimpleROM t_LeftUpperArm = squeletteRom[BodyStructureMap.SubSegmentTypes.SubsegmentType_LeftUpperArm];
+            t_LeftUpperArm.SetPitchMinMax(-60, 100); // up/down
+            t_LeftUpperArm.SetYawMinMax(-100, 100);  // front/back  
+            t_LeftUpperArm.SetRollMinMax(-90, 90);   // twist
+            SimpleROM t_LeftForeArm = squeletteRom[BodyStructureMap.SubSegmentTypes.SubsegmentType_LeftForeArm];
+            t_LeftForeArm.SetYawMinMax(-80, 80);  // flex 
+            t_LeftForeArm.SetRollMinMax(-20, 150);   // twist
+
+            SimpleROM t_RightThigh = squeletteRom[BodyStructureMap.SubSegmentTypes.SubsegmentType_RightThigh];
+            t_RightThigh.SetPitchMinMax(-90, 50); // up/down
+            t_RightThigh.SetYawMinMax(-60, 60);  //right/left  
+            t_RightThigh.SetRollMinMax(-60, 60);   // twist
+            SimpleROM t_RightCalf = squeletteRom[BodyStructureMap.SubSegmentTypes.SubsegmentType_RightCalf];
+            t_RightCalf.SetYawMinMax(-80, 80);  // flex 
+            t_RightCalf.SetRollMinMax(-90, 90);   // twist
+
+            SimpleROM t_LeftThigh = squeletteRom[BodyStructureMap.SubSegmentTypes.SubsegmentType_LeftThigh];
+            t_LeftThigh.SetPitchMinMax(-90, 50); // up/down
+            t_LeftThigh.SetYawMinMax(-60, 60);  //right/left  
+            t_LeftThigh.SetRollMinMax(-60, 60);   // twist
+            SimpleROM t_LeftCalf = squeletteRom[BodyStructureMap.SubSegmentTypes.SubsegmentType_LeftCalf];
+            t_LeftCalf.SetYawMinMax(-80, 80);  // flex 
+            t_LeftCalf.SetRollMinMax(-90, 90);   // twist
+        }
+
+        public void capFrame()
+        {
+
+        }
+    }
+}
