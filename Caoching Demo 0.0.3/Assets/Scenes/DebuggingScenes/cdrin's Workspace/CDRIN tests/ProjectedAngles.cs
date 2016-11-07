9397e570b3b067a2d6375efb3df69e4370ce0348
Jeremie Kaltenmark <kaltenmark.jeremie@cdrin.com>
2016-11-07 10:12:00 -0500

    add test scene for DOF application

---
+++ Caoching Demo 0.0.3/Assets/Scenes/DebuggingScenes/cdrin's Workspace/CDRIN tests/ProjectedAngles.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectedAngles : MonoBehaviour
+{
+    public Vector3 SetParentEuler;
+    public Vector3 SetReferenceEuler;
+    public Vector3 m_ReferenceProjectedAngles;
+    public Vector3 m_WantedAngles;
+    public Vector3 currentEuler;
+    //public Vector3 m_CurrentProjectedLocalAngles;
+
+    public Transform other;
+    
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.parent.rotation = Quaternion.Euler(SetParentEuler);
+        other.rotation = Quaternion.Euler(SetReferenceEuler);
+        ExtractAngles(other.localRotation, ref m_ReferenceProjectedAngles);
+        //m_WantedAngles = m_ReferenceProjectedAngles;
+        //transform.localRotation = ComputeQuaternion(m_WantedAngles);
+        //currentEuler = transform.rotation.eulerAngles;
+    }
+
+    public static void ExtractAngles(Quaternion a_QuatLocal, ref Vector3 a_Angles)
+    {
+        Vector3 localForward = a_QuatLocal * Vector3.forward;
+        Vector3 localRight = a_QuatLocal * Vector3.right;
+        Vector3 localUp = a_QuatLocal * Vector3.up;
+
+        Vector3 upProj = Vector3.ProjectOnPlane(localUp, Vector3.right)     ;   // pitch angle
+        Vector3 forwardProj   = Vector3.ProjectOnPlane(localForward, Vector3.up)   ;        //yaw angle 
+        Vector3 rightProj  = Vector3.ProjectOnPlane(localRight, Vector3.forward);  // roll angle
+
+        upProj.Normalize();
+        forwardProj.Normalize()  ;
+        rightProj.Normalize();
+
+        a_Angles.x = Vector3.Angle(Vector3.up, upProj);
+        a_Angles.y = Vector3.Angle(Vector3.forward, forwardProj);
+        a_Angles.z = Vector3.Angle(Vector3.right, rightProj);
+    }
+
+    public static Quaternion ComputeQuaternion(Vector3 a_WantedAngles)
+    {
+        //Vector3 localForward = Vector3.forward;
+        //Vector3 localRight = Vector3.right;
+        //Vector3 localUp = Vector3.up;
+        //
+        //localUp.y = Mathf.Cos(a_WantedAngles.x * Mathf.Deg2Rad);
+        //localUp.z = Mathf.Sin(a_WantedAngles.x * Mathf.Deg2Rad);
+        //
+        //localForward.x = Mathf.Sin(a_WantedAngles.y * Mathf.Deg2Rad);
+        //localForward.z = Mathf.Cos(a_WantedAngles.y * Mathf.Deg2Rad);
+        //
+        //localRight.x = Mathf.Cos(a_WantedAngles.z * Mathf.Deg2Rad);
+        //localRight.y = Mathf.Sin(a_WantedAngles.z * Mathf.Deg2Rad);
+        //
+        //
+        //Vector3 tt = Vector3.Cross(localUp, localForward);
+        //
+        //Quaternion tret = Quaternion.LookRotation(localForward, localUp);
+        //
+        //Quaternion tret = Quaternion.Euler(0, a_WantedAngles.y, 0) * Quaternion.Euler(a_WantedAngles.x, 0, 0) * Quaternion.Euler(0, 0, a_WantedAngles.z);
+        Quaternion tret = Quaternion.Euler(a_WantedAngles);
+        return tret;
+    }
+
+
+}
---
+++ Caoching Demo 0.0.3/Assets/Scenes/DebuggingScenes/cdrin's Workspace/CDRIN tests/Quatconstraint.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Quatconstraint : MonoBehaviour
+{
+    ProjectedAngles m_ProjectedAngles;
+    public Vector3 m_AnglesAnalysis;
+    public Vector2 XminMax = new Vector2(-30, 30);
+    public Vector2 YminMax = new Vector2(-30, 30);
+    public Vector2 ZminMax = new Vector2(-30, 30);
+
+    ///*public*/ Vector3 axis;
+    ///*public*/ float angle;
+
+    //Quaternion Xmin, Xmax;
+    //Quaternion Ymin, Ymax;
+    //Quaternion Zmin, Zmax;
+
+    public Vector3 eulerBegin = new Vector3(0, 0, 150);
+    public Vector3 eulerEnd = new Vector3(0, 0, 150);
+    private Quaternion EndQuat;
+    private Quaternion beginQuat;
+    public Quaternion CurrentQuat;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        beginQuat = transform.localRotation;
+        eulerBegin = beginQuat.eulerAngles;
+
+        //Xmin = Quaternion.AngleAxis(XminMax.x, transform.right);
+        //Xmax = Quaternion.AngleAxis(XminMax.y, transform.right);
+        //Ymin = Quaternion.AngleAxis(YminMax.x, transform.up);
+        //Ymax = Quaternion.AngleAxis(YminMax.y, transform.up);
+        //Zmin = Quaternion.AngleAxis(ZminMax.x, transform.forward);
+        //Zmax = Quaternion.AngleAxis(ZminMax.y, transform.forward);
+
+        if (m_ProjectedAngles == null)
+            m_ProjectedAngles = gameObject.GetComponent<ProjectedAngles>();
+    }
+
+    static int i = 0;
+    bool pos = true;
+
+    private Quaternion Animate()
+    {
+        beginQuat = Quaternion.Euler(eulerBegin);
+        EndQuat = Quaternion.Euler(eulerEnd);
+
+        if (pos) ++i;
+        else --i;
+        //EndQuat = Quaternion.Euler(eulerEnd);
+        Quaternion tQuat = Quaternion.Slerp(beginQuat, EndQuat, i * 0.01f);
+
+        if (Quaternion.Angle(tQuat, EndQuat) < 3) pos = false;
+        else if (Quaternion.Angle(tQuat, beginQuat) < 3) pos = true;
+
+        return tQuat;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion tQuat = Animate();
+        ProjectedAngles.ExtractAngles(tQuat,ref m_AnglesAnalysis);
+        CurrentQuat = tQuat * Constraint(m_AnglesAnalysis);
+
+        transform.localRotation = CurrentQuat; // Constraint(m_ProjectedAngles.m_ReferenceProjectedAngles);
+    }
+
+    Quaternion Constraint(Vector3 a_ProjAngles)
+    {
+
+        ////currentEuler = aQuat.eulerAngles;
+        //ExtractAngles(aQuat, ref ProjectedAngles);
+        a_ProjAngles = ClampAngle(a_ProjAngles);
+        Quaternion tRet = Quaternion.Euler(a_ProjAngles);
+
+        return tRet;
+    }
+
+    private Vector3 ClampAngle(Vector3 a_ProjAngles)
+    {
+        Vector3 offset = Vector3.zero;
+        if      (a_ProjAngles.x > XminMax.y)
+                       offset.x = XminMax.y - a_ProjAngles.x;
+        else if (a_ProjAngles.x < XminMax.x)
+                       offset.x = XminMax.x - a_ProjAngles.x;
+        else           offset.x = 0;
+
+        if      (a_ProjAngles.y > YminMax.y)
+                       offset.y = YminMax.y - a_ProjAngles.y;
+        else if (a_ProjAngles.y < YminMax.x)
+                       offset.y = YminMax.x - a_ProjAngles.y;
+        else           offset.y = 0;
+
+        if      (a_ProjAngles.z > ZminMax.y)
+                       offset.z = ZminMax.y - a_ProjAngles.z;
+        else if (a_ProjAngles.z < ZminMax.x)
+                       offset.z = ZminMax.x - a_ProjAngles.z;
+        else           offset.z = 0;
+
+        return offset;
+//         if (currentEuler.x > MaxEulerConsttraint.x) { currentEuler.x = MaxEulerConsttraint.x; pos = !pos; }
+//         if (currentEuler.y > MaxEulerConsttraint.y) { currentEuler.y = MaxEulerConsttraint.y; pos = !pos; }
+//         if (currentEuler.z > MaxEulerConsttraint.z) { currentEuler.z = MaxEulerConsttraint.z; pos = !pos; }
+
+
+    }
+
+
+}
+
+
