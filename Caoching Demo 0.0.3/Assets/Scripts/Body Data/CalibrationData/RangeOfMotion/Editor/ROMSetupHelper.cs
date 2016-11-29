8e1a93205c23eed4f7eb91bb7d53573fb924bec9
Jeremie Kaltenmark <kaltenmark.jeremie@cdrin.com>
2016-11-29 13:51:34 -0500

    correction on orthonormal repere for quaternion representation

--- Caoching Demo 0.0.3/Assets/Scripts/Body Data/CalibrationData/RangeOfMotion/Editor/ROMSetupHelper.cs
+++ Caoching Demo 0.0.3/Assets/Scripts/Body Data/CalibrationData/RangeOfMotion/Editor/ROMSetupHelper.cs
@@ -12,10 +12,16 @@ public class ROMSetupHelper : Editor
     Color MainCol = Color.magenta;
     Color orthoCol = new Color(0.5f, 0.5f, 0.5f, 1.0f);
     Color angleCol = Color.yellow;
+
+    public bool isArm;
+    public bool isRight;
+
+    public bool drawOrtho = false;
+
     void OnSceneGUI()
     {
         DummyQuaternionHelper segment = target as DummyQuaternionHelper;
-        float arrowSize = HandleUtility.GetHandleSize(segment.transform.position)*1.2f;
+        float arrowSize = HandleUtility.GetHandleSize(segment.transform.position) * 1.2f;
         float offsetSize = HandleUtility.GetHandleSize(segment.transform.position) * 1.1f;
         float radiusSize = HandleUtility.GetHandleSize(segment.transform.position) * 0.5f;
 
@@ -41,28 +47,67 @@ public class ROMSetupHelper : Editor
         Quaternion t_rot = Quaternion.LookRotation(axis, t_up);
         Handles.ArrowCap(0, segment.transform.position, t_rot, arrowSize);
 
-
         // ortho Arrow
-        if(Mathf.Sin(Vector3.Angle(axis, Vector3.forward) * Mathf.Deg2Rad) < 0.1f) // special case if rotation axis is near +/- forward
+        if (drawOrtho)
         {
-
+            Quaternion t_orthoQ = Quaternion.LookRotation(t_orthoVec, t_up);
             Handles.color = orthoCol;
-            Handles.ArrowCap(0, segment.transform.position, tQuat * Quaternion.Euler(0,90,0), arrowSize);
-            Handles.color = angleCol;
-            Handles.DrawSolidArc(segment.transform.position + axis * offsetSize, axis, Vector3.right, angle, radiusSize);
+            Handles.ArrowCap(0, segment.transform.position, t_orthoQ, arrowSize);
         }
-        else // trivial case
+
+        //local segment direction
+        if (isArm)
         {
-
-            Handles.color = orthoCol;
-            Handles.ArrowCap(0, segment.transform.position, tQuat, arrowSize);
-            Handles.color = angleCol;
-            Handles.DrawSolidArc(segment.transform.position + axis * offsetSize, axis, tQuat * Vector3.forward, angle, radiusSize);
+            Handles.color = Handles.xAxisColor;
+            if (isRight)
+                Handles.ArrowCap(0, segment.transform.position, segment.transform.rotation * Quaternion.Euler(0, 90, 0), arrowSize);
+            else
+                Handles.ArrowCap(0, segment.transform.position, segment.transform.rotation * Quaternion.Euler(0, -90, 0), arrowSize);
         }
+        else //local up
+        {
+            Handles.color = Handles.yAxisColor;
+            Handles.ArrowCap(0, segment.transform.position, segment.transform.rotation * Quaternion.Euler(90, 0, 0), arrowSize);
+        }
+
+
+        //angle
+        Handles.color = angleCol;
+        Handles.DrawSolidArc(segment.transform.position + axis * offsetSize, axis, t_orthoVec, angle, radiusSize);
     }
 
-
-    void OnEnable()
+    public void OnEnable()
     {
+        DummyQuaternionHelper segment = target as DummyQuaternionHelper;
+        if (segment.transform.name.Contains("Leg"))
+            isArm = false;
+        else
+            isArm = true;
+
+        if (segment.transform.name.Contains("Left"))
+            isRight = false;
+        else
+            isRight = true;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        if (isArm)
+        {
+            if (isRight)
+                EditorGUILayout.LabelField("Right Arm");
+            else
+                EditorGUILayout.LabelField("Left Arm");
+
+        }
+        else
+            if (isRight)
+            EditorGUILayout.LabelField("Right Leg");
+        else
+            EditorGUILayout.LabelField("Left Leg");
+
+        drawOrtho = EditorGUILayout.Toggle("Draw Orthogonal vector (grey)", drawOrtho);
+
+
     }
 }
